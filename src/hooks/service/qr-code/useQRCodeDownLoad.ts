cbc7f73cc873b1772dce2822f6821eb377070cc7
LeeDongTak <sunny5796@naver.com>
2024-10-18 16:44:53 +0900

    함수이름 변경으로인한 에러처리

--- src/hooks/service/qr-code/useQRCodeDownLoad.ts
+++ src/hooks/service/qr-code/useQRCodeDownLoad.ts
@@ -1,16 +1,16 @@
-import { QRDownload, QRDownloadAll } from '@/server/api/supabase/qrCodeDownLoad';
+import { QRDownload, QRDownloadAllCustomFeatureFor } from '@/server/api/supabase/qrCodeDownLoad';
 import { useMutation } from '@tanstack/react-query';
 
 const useQRCodeDownLoad = () => {
- const {mutate: AllMutate, isPending: AllIsPending} = useMutation({
-  mutationFn: QRDownloadAll
- })
+  const { mutate: AllMutate, isPending: AllIsPending } = useMutation({
+    mutationFn: QRDownloadAllCustomFeatureFor,
+  });
 
- const {mutate: oneMutate, isPending: oneIsPending} = useMutation({
-  mutationFn: QRDownload
- })
+  const { mutate: oneMutate, isPending: oneIsPending } = useMutation({
+    mutationFn: QRDownload,
+  });
 
- return {AllMutate, AllIsPending, oneMutate, oneIsPending}
+  return { AllMutate, AllIsPending, oneMutate, oneIsPending };
 };
 
 export default useQRCodeDownLoad;
