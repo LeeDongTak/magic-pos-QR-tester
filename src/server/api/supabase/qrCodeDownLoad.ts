927744a0cd4e7d384c2d6c92a283a5dbd753bd41
LeeDongTak <sunny5796@naver.com>
2024-10-18 16:46:55 +0900

    custom기능 개발 1차

--- src/server/api/supabase/qrCodeDownLoad.ts
+++ src/server/api/supabase/qrCodeDownLoad.ts
@@ -1,35 +1,131 @@
 import { QRdataType } from '@/shared/store/qrCode';
 import domtoimage from 'dom-to-image';
 import { saveAs } from 'file-saver';
+import html2canvas from 'html2canvas';
 import JSZip from 'jszip';
 
-// const logMemoryUsage = () => {
-//   const usedJSHeap = window.performance.memory;
-//   const totalJSHeap = window.performance.memory.totalJSHeapSize / 1024 / 1024;
-//   const jsHeapLimit = window.performance.memory.jsHeapSizeLimit / 1024 / 1024;
-//   console.log(`Used JS Heap: ${usedJSHeap.toFixed(2)} MB`);
-//   console.log(`Total JS Heap: ${totalJSHeap.toFixed(2)} MB`);
-//   console.log(`JS Heap Limit: ${jsHeapLimit.toFixed(2)} MB`);
-//   // } else {
-//   //   console.log('The browser does not support performance.memory API.');
-//   // }
-// };
+export const QRDownloadAllHtml2CanvasPromiseAll = async (qrData: QRdataType[]) => {
+  if (qrData) {
+    try {
+      const startTime = window.performance.now();
+      const zip = new JSZip();
+      const qrCodeImg = qrData.map(async qrCode => {
+        // html2canvas
+        const canvas = await html2canvas(qrCode.qrRef, { scale: 2 });
+        const image = canvas.toDataURL('image/png');
+        return zip.file(`${qrCode.orderType}.jpg`, image.split(',')[1], { base64: true });
+      });
 
-export const QRDownloadAll = async (qrData: QRdataType[]) => {
+      await Promise.all(qrCodeImg).then(() => {
+        zip
+          .generateAsync({ type: 'blob' })
+          .then(content => {
+            saveAs(content, 'QRCode');
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      });
+      const endTime = window.performance.now();
+      console.log(`html2canvas + promiseAll => ${(endTime - startTime).toFixed(2)} ms`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+export const QRDownloadAllHtml2CanvasFor = async (qrData: QRdataType[]) => {
+  if (qrData) {
+    try {
+      const startTime = window.performance.now();
+      const zip = new JSZip();
+      for (let i = 0; i < qrData.length; i++) {
+        // html2canvas
+        const canvas = await html2canvas(qrData[i].qrRef, { scale: 2 });
+        const image = canvas.toDataURL('image/png');
+        zip.file(`${qrData[i].orderType}.jpg`, image.split(',')[1], { base64: true });
+      }
+
+      zip
+        .generateAsync({ type: 'blob' })
+        .then(content => {
+          saveAs(content, 'QRCode');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+      const endTime = window.performance.now();
+      console.log(`html2canvas + for => ${(endTime - startTime).toFixed(2)} ms`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+////////////////////////
+export const QRDownloadAllDomToImagePromiseAll = async (qrData: QRdataType[]) => {
   if (qrData) {
     try {
       const startTime = window.performance.now();
       const zip = new JSZip();
       const qrCodeImg = qrData.map(async qrCode => {
         // domToImage
-        // const file = await domtoimage.toBlob(qrCode.qrRef);
-        // return zip.file(`${qrCode.orderType}.jpg`, file);
+        const file = await domtoimage.toBlob(qrCode.qrRef);
+        return zip.file(`${qrCode.orderType}.jpg`, file);
+      });
 
-        // html2canvas
-        // const canvas = await html2canvas(qrCode.qrRef, { scale: 2 });
-        // const image = canvas.toDataURL('image/png');
-        // return zip.file(`${qrCode.orderType}.jpg`, image.split(',')[1], { base64: true });
+      await Promise.all(qrCodeImg).then(() => {
+        zip
+          .generateAsync({ type: 'blob' })
+          .then(content => {
+            saveAs(content, 'QRCode');
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      });
+      const endTime = window.performance.now();
+      console.log(`DomToImage + promiseAll => ${(endTime - startTime).toFixed(2)} ms`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+export const QRDownloadAllDomToImageFor = async (qrData: QRdataType[]) => {
+  if (qrData) {
+    try {
+      const startTime = window.performance.now();
+      const zip = new JSZip();
+      for (let i = 0; i < qrData.length; i++) {
+        // domToImage
+        const file = await domtoimage.toBlob(qrData[i].qrRef);
+        zip.file(`${qrData[i].orderType}.jpg`, file);
+      }
 
+      zip
+        .generateAsync({ type: 'blob' })
+        .then(content => {
+          saveAs(content, 'QRCode');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+      const endTime = window.performance.now();
+      console.log(`DomToImage + for => ${(endTime - startTime).toFixed(2)} ms`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+///////////////////////////////////
+export const QRDownloadAllCustomFeaturePromiseAll = async (qrData: QRdataType[]) => {
+  if (qrData) {
+    try {
+      const startTime = window.performance.now();
+      const zip = new JSZip();
+      const qrCodeImg = qrData.map(async qrCode => {
         // custom feature
         const canvas = await domToCanvas(qrCode.qrRef);
         const image = canvas.toDataURL('image/png');
@@ -47,23 +143,54 @@ export const QRDownloadAll = async (qrData: QRdataType[]) => {
           });
       });
       const endTime = window.performance.now();
-      console.log(`${(endTime - startTime).toFixed(2)} ms`);
+      console.log(`CustomFeature + promiseAll => ${(endTime - startTime).toFixed(2)} ms`);
     } catch (error) {
       console.error(error);
     }
   }
 };
+export const QRDownloadAllCustomFeatureFor = async (qrData: QRdataType[]) => {
+  if (qrData) {
+    try {
+      const startTime = window.performance.now();
+      const zip = new JSZip();
+      for (let i = 0; i < qrData.length; i++) {
+        // custom feature
+        const canvas = await domToCanvas(qrData[i].qrRef);
+        const image = canvas.toDataURL('image/png');
+        zip.file(`${qrData[i].orderType}.jpg`, image.split(',')[1], { base64: true });
+      }
+
+      zip
+        .generateAsync({ type: 'blob' })
+        .then(content => {
+          saveAs(content, 'QRCode');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+      const endTime = window.performance.now();
+      console.log(`CustomFeature + for => ${(endTime - startTime).toFixed(2)} ms`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
 const applyInlineStyles = (element: HTMLElement) => {
   const computedStyle = window.getComputedStyle(element);
   let inlineStyles = '';
-
   Array.from(computedStyle).forEach(key => {
     inlineStyles += `${key}: ${computedStyle.getPropertyValue(key)}; `;
   });
-
-  element.setAttribute('style', inlineStyles); // 한 번에 적용
-
+  element.setAttribute('style', inlineStyles);
   Array.from(element.children).forEach((child: Element) => applyInlineStyles(child as HTMLElement));
+};
+
+const removeInlineStyles = (element: HTMLElement) => {
+  element.removeAttribute('style');
+  Array.from(element.children).forEach(child => removeInlineStyles(child as HTMLElement));
 };
 
 const domToCanvas = (element: HTMLElement): Promise<HTMLCanvasElement> => {
@@ -88,6 +215,7 @@ const domToCanvas = (element: HTMLElement): Promise<HTMLCanvasElement> => {
       context?.drawImage(image, 0, 0);
       resolve(canvas);
     };
+    removeInlineStyles(element);
   });
   return canvasEl;
 };
